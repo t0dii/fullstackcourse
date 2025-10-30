b055820ccfe2d83d48380ba096b10074bdbc8098
Ethan Li <elii0022@student.monash.edu>
2025-10-30 19:56:42 +1100

    Finished up to part 1.5.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,71 +1,28 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        This course is {props.course}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        The total number of exercises are {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-    </div>
-
-  )
-}
-
-const Part = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        For {props.name} there are {props.exercises} exercises
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part name={props.part1} exercises={props.exercises1}/>
-      <Part name={props.part2} exercises={props.exercises2}/>
-      <Part name={props.part3} exercises={props.exercises3}/>
-    </div>
-  )
-}
-
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content
-      part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2}
-      part3={part3} exercises3={exercises3}
-      />
-      <Total
-      exercises1={exercises1}
-      exercises2={exercises2}
-      exercises3={exercises3}
-      />
+      <p>Title of course: {course.name}</p>
+      <p>Name of course: {course.parts[0].name}. Number of exercises: {course.parts[0].exercises}</p>
+      <p>Name of course: {course.parts[1].name}. Number of exercises: {course.parts[1].exercises}</p>
+      <p>Name of course: {course.parts[2].name}. Number of exercises: {course.parts[2].exercises}</p>
     </div>
   )
 }
