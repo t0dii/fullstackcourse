0b70794f0cffac3b908afa9ef73218dcd30e6107
Ethan Li <elii0022@student.monash.edu>
2025-11-03 19:49:51 +1100

    finished part1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,15 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [good, setGood] = useState(0)
-  const [netural, setNeutral] = useState(0)
+  const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
 
   return (
     <div>
       <h1>give feedback</h1>
-      
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
       <h1>statistics</h1>
+      <div>good={good}</div>
+      <div>bad={bad}</div>
+      <div>neutral={neutral}</div>
     </div>
   )
 }
