a340c3f2a9dcfc4e4cea7a69075fab2864f255b6
Ethan Li <elii0022@student.monash.edu>
2025-11-05 00:09:20 +1100

    finished task 1.7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,12 +4,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + bad + neutral;
 
 
 
   return (
     <div>
       <h1>give feedback</h1>
+
       <button onClick={() => setGood(good + 1)}>
         good
       </button>
@@ -17,13 +19,19 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>
         bad
       </button>
+
       <button onClick={() => setNeutral(neutral + 1)}>
         neutral
       </button>
+
       <h1>statistics</h1>
+
       <div>good={good}</div>
       <div>bad={bad}</div>
       <div>neutral={neutral}</div>
+      <div>average={((good*1 + bad*-1 + neutral*0)/all)}</div>
+      <div>positive={(good/all)*100 + '%'}</div>
+
     </div>
   )
 }
