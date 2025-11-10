f59f9ba67f2ab896ac3949d63558573c886ef330
Ethan Li <elii0022@student.monash.edu>
2025-11-10 16:30:54 +1100

    Finished 1.12*

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -47,6 +47,19 @@ const App = () => {
   const all = good + bad + neutral;
   const average = (good*1 + bad*-1 + neutral*0)/all;
   const positive = (good/all)*100 + '%';
+  const [selected, setSelected] = useState(0)
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
 
   const neutralClick = () => {
     setAll(allClicks.concat(''))
@@ -60,7 +73,9 @@ const App = () => {
     setAll(allClicks.concat(''))
     setGood(good + 1)
   }
-
+  const arrayClick = () => {
+    setSelected(selected + 1)
+  }
   return (
     <div>
       <h1>give feedback</h1>
@@ -90,6 +105,10 @@ const App = () => {
         </tbody>
       </table>
 
+      <h1>Anecdotes</h1>
+      {anecdotes[selected]}
+      <br/>
+      <button onClick={arrayClick}>next anecdotes</button>
 
     </div>
   )
