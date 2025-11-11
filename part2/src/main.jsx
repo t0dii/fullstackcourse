171b7cf44e3c1549885e3235e6dfba0483b400bb
Ethan Li <elii0022@student.monash.edu>
2025-11-11 15:47:46 +1100

    Set up my project directories properly for part2

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//Once finished with the current directory, change it to fit the next one. E.g. './phonebook/App'
+import App from './courseinfo/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
