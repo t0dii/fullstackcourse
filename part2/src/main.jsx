067bf92a71f3e352c96c51edab356dc20e65da3a
Ethan Li <elii0022@student.monash.edu>
2025-11-22 22:35:01 +1100

    doing exercise 2.6

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
 //Once finished with the current directory, change it to fit the next one. E.g. './phonebook/App'
-import App from './courseinfo/App'
+import App from './phonebook/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
