1133b316d9415f9107373be92840f1e17b363ad7
Ethan Li <elii0022@student.monash.edu>
2025-12-01 15:36:06 +1100

    Continuing with part 2.6

--- part2/src/phonebook/App.jsx
+++ part2/src/phonebook/App.jsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState("");
+  const [newName, setNewName] = useState("");
+
+  const handleInput = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const noteObject = {
+      content: newName,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      name:
+      <form onSubmit={addNote}>
+        <input value={newName} onChange={handleInput} />
+        <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.name}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
