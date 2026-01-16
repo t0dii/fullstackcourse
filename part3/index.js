0fcd18a5fb835a8912f47a7aadb34a8f37133b12
Ethan Li <elii0022@student.monash.edu>
2026-01-16 18:50:49 +1100

    completed part 3.2

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,18 @@ app.get("/api/persons", (request, response) => {
   response.json(notes);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${notes.length} people </p> <p> ${new Date()}</p>`,
+  );
+});
+
+app.post("/info", (request, response) => {
+  const note = request.body;
+  console.log(note);
+  response.json(note);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
