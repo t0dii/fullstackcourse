b7a257af59d6664e25ee06803881036420375764
Ethan Li <elii0022@student.monash.edu>
2026-01-16 18:18:47 +1100

    part 3.1 completed.

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
 let notes = [
   {
@@ -29,33 +28,8 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/persons", (request, response) => {
   response.json(notes);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  response.json(note);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
-  response.json(note);
 });
 
 const PORT = 3001;
