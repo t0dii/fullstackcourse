fc8249049d0734cfd8af81c8fd348a92c4ec9e42
Ethan Li <elii0022@student.monash.edu>
2026-01-30 17:14:54 +1100

    .

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 let notes = [
   {
@@ -92,7 +94,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
